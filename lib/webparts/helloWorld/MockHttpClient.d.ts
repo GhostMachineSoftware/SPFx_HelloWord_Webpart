7758408b52026e366ba9ad390bfd6c2605fe3598
Jason Clark <jason_mc@btinternet.com>
2019-11-30 22:09:44 +0000

    added core files

---
+++ lib/webparts/helloWorld/MockHttpClient.d.ts
@@ -0,0 +1,6 @@
+import { ISPList } from './HelloWorldWebPart';
+export default class MockHttpClient {
+    private static _items;
+    static get(): Promise<ISPList[]>;
+}
+//# sourceMappingURL=MockHttpClient.d.ts.map
